edd977e26e2ccd62db9eda4fe18259b5a21dc280
bshao001 <bshao001@gmail.com>
2017-05-29 18:05:17 -0400

    Add detect_and_classify method.

--- mesgclsf/msgclassifier.py
+++ mesgclsf/msgclassifier.py
@@ -3,18 +3,17 @@ import tensorflow as tf
 from collections import Counter
 
 from misc.cnnpredictor import CnnPredictor
+from textdect.textdetector import detect
 from mesgclsf.imageresizer import resize_to_desired
 from mesgclsf.mctrainer import FEATURE_HEIGHT, FEATURE_WIDTH
 
 
-def classify(session, result_dir, result_file, img_arr, stride=16):
+def classify(classifier, img_arr, stride=16):
     """
-    Take an image file, read and analyze the image, and returns the class ID of the
-    input image based on the message content on it.
+    Take an image file (an output from the first step), read and analyze the image, and 
+    returns the class ID of the input image based on the message content on it.
     Args:
-        session: The TensorFlow session used to run the prediction.
-        result_dir: The full path to the folder in which the result file locates.
-        result_file: The file that saves the training results.
+        classifier: A CnnPredictor constructed based on a trained model for step 2.
         img_arr: A numpy ndarray that holds the image features.
         stride: Optional. The stride of the sliding.
     Returns:
@@ -30,11 +29,10 @@ def classify(session, result_dir, result_file, img_arr, stride=16):
 
     features = []
     for x in range(0, img_width - FEATURE_WIDTH + 1, stride):
-        this_win = img_arr[:, x:x + FEATURE_WIDTH]
+        this_win = resized_img[:, x:x + FEATURE_WIDTH]
         features.append(this_win.reshape(-1))
 
-    cnn_pred = CnnPredictor(session, result_dir, result_file)
-    _, indices = cnn_pred.predict(np.asarray(features))
+    _, indices = classifier.predict(np.asarray(features))
 
     img_cnt = len(features)
 
@@ -48,6 +46,22 @@ def classify(session, result_dir, result_file, img_arr, stride=16):
     return class_id, confidence
 
 
+def detect_and_classify(result_dir, detect_file, classify_file, gray_array):
+    with tf.Session() as s1:
+        detector = CnnPredictor(s1, result_dir, detect_file)
+        areas, _ = detect(detector, gray_array)
+
+    tf.reset_default_graph()
+    with tf.Session() as s2:
+        classifier = CnnPredictor(s2, result_dir, classify_file)
+        for area in areas:
+            y, x, h, w = area[0], area[1], area[2], area[3]
+            x2, y2 = x + w, y + h
+
+            area_img = gry_arr[y:y2, x:x2]
+            cls_id, conf = classify(classifier, area_img)
+            print("class ID: {}, confidence: {}".format(cls_id, conf))
+
 if __name__ == "__main__":
     import os
     import matplotlib.pyplot as plt
@@ -56,13 +70,18 @@ if __name__ == "__main__":
     from time import time
 
     from settings import PROJECT_ROOT
-    from misc.imgtools import plot_samples
 
     t0 = time()
     res_dir = os.path.join(PROJECT_ROOT, 'Data', 'Result')
-    img_file_name = os.path.join(PROJECT_ROOT, 'Data', 'Step1', 'Test', 'sign14.jpg')
-    # with tf.Session() as sess:
-    #     areas, pos_a = detect(sess, res_dir, 'step1_deep_v2', img_file_name)
+    img_file = os.path.join(PROJECT_ROOT, 'Data', 'Step1', 'Test', 'sign1.jpg')
+    img_arr = skio.imread(img_file)
+    gry_arr = skcolor.rgb2gray(img_arr)
+
+    detect_and_classify(sess, res_dir, 'step1_dcnn', 'step2_tas_basic', gry_arr)
+
     t1 = time()
     print("Running time: {:4.2f} seconds".format(t1-t0))
+
+    plt.imshow(img_arr)
+    plt.show()
 
